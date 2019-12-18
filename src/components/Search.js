0c380e9ccec115b3313f40f4e3d007dbf2f1dc4f
Mira <calopter@gmail.com>
2019-12-18 11:06:15 -0800

    basic search functionality

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+
+import MovieList from './MovieList';
 
 class Search extends Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      query: '',
+      movies: [],
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({ query: event.target.value })
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault()
+    
+    axios.get(`http://localhost:3000/movies?query=${this.state.query}`)
+      .then(response => {
+        console.log(response.data)
+        this.setState({ movies: response.data })
+      }).catch(console.log)
   }
 
   render() {
     return (
-      'Search'
+      <section>
+        <form onSubmit={this.onSubmit}>
+          <input type='text' placeholder='search' value={this.state.query} onChange={this.onChange}/>
+          <input type='submit' value='Search' className='btn btn-primary'/>
+        </form>
+        <MovieList movies={this.state.movies} />
+      </section>
     )
   }
 }
